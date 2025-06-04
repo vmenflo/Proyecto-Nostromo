30cad42f299a024e92fbcf9d95d76285fca717fe
Victor <menaflores.victor@gmail.com>
2025-06-04 18:46:05 +0200

    reestructurar

--- servicios_rest/index.php
+++ servicios_rest/index.php
@@ -7,6 +7,23 @@ require "src/funciones_CTES_servicios.php";
 
 $app = new \Slim\App;
 
+// Middleware CORS
+$app->add(function ($request, $response, $next) {
+    $response = $next($request, $response);
+    return $response
+        ->withHeader("Access-Control-Allow-Origin", "*")
+        ->withHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept, Origin, Authorization")
+        ->withHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+});
+
+// Preflight requests
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    exit(0);
+}
+
 // Control logueado
 $app->get('/logueado', function () {
     $test = validateToken();
@@ -280,23 +297,6 @@ $app->get('/admin/repetido/{tabla}/{columna}/{valor}', function ($request) {
     else
         echo json_encode(array("no_auth" => "No tienes permisos para usar este servicio"));
 });
-
-// Middleware CORS
-$app->add(function ($request, $response, $next) {
-    $response = $next($request, $response);
-    return $response
-        ->withHeader("Access-Control-Allow-Origin", "*")
-        ->withHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept, Origin, Authorization")
-        ->withHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-});
-
-// Preflight requests
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    exit(0);
-}
 
 $app->run();
 ?>
